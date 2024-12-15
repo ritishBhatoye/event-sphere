eb8b24c4dd62caff66e928569e3e78e900dc103a
Ritish Bhatoye <ritishrahul87@gmail.com>
2024-12-16 00:49:54 +0530

    color config

--- components/global/welcome/SignUpScreen.tsx
+++ components/global/welcome/SignUpScreen.tsx
@@ -1,25 +1,83 @@
 import { TextField } from '@/components/atoms/TextField';
 import React, { useState } from 'react'
-import { View } from 'react-native';
+import { View,Text } from 'react-native';
 
 
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 
 export const SignUpScreen = () => {
-  const [email,setEmai]=useState('');
-  return (
-    <SafeAreaView>
-      <View className="p-4">
 
-      
-      <TextField
-      onChangeText={setEmai}
-      placeholder='Enter your email'
-      keyboardType='email-address'
-      label="Email"
-      value={email}
-      className='mb-4 '
+  const[firstName,setFirstName]=useState('');
+  const[lastName,setLastName]=useState('');
+  const[email,setEmail]=useState('')
+  const[phoneNumber,setPhoneNumber]=useState('');
+  const [password,setPassword]=useState('');
+  const[confirmPassword,setConfirmPassword]=useState('');
+  return (
+    <SafeAreaView className='w-full h-full'>
+<View className='relative top-20'>
+<Text className='text-light-secondary text-5xl font-bold text-center '>
+              SIGN UP
+        </Text>
+</View>
+      <View className="gap-6 px-6 relative top-[15%] h-full">
+     
+        <View className='flex-row gap-4 w-full justify-between '>
+          <View className='flex-1'>
+      <TextField 
+      value={firstName} 
+      onChangeText={setFirstName}
+      placeholder='Enter your first name'
+      label="First Name"
+      keyboardType='default'
+      // className='mb-4 '
+      />
+      </View>
+
+      <View className='flex-1'>
+      <TextField 
+      value={lastName} 
+      onChangeText={setLastName}
+      placeholder='Enter your last name'
+      label="Last Name"
+      keyboardType='default'
+      // className='mb-4 '
+      />
+</View>
+</View>
+     
+       <TextField
+       onChangeText={setEmail}
+       placeholder='Enter your email'
+       keyboardType='default'
+       label="Email"
+       value={email}
+       className='mb-4 '
+       />
+       <TextField
+       onChangeText={setPhoneNumber}
+       placeholder='Enter your phone number'
+       keyboardType='default'
+       label="Phone Number"
+       value={phoneNumber}
+       className='mb-4 '
+       />
+       <TextField
+       onChangeText={setPassword}
+       placeholder='Enter your password'
+       keyboardType='default'
+       label="Password"
+       value={password}
+       className='mb-4 '
+       />
+<TextField
+       onChangeText={setConfirmPassword}
+       placeholder='Enter your password'
+       keyboardType='default'
+       label="Confirm Password"
+       value={confirmPassword}
+       className='mb-4 '
        />
        </View>
        
